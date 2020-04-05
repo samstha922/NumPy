04ed74d0c52254fd4854ca63e7fd60010402b1ab
sampresh <slevinshrestha@gmail.com>
2020-04-06 07:47:22 +1000

    linear regression Added

--- numpy1.py
+++ numpy1.py
@@ -67,3 +67,21 @@ plt.show()
 # sbs.distplot(x, hist=True)
 # plt.show()
 
+# ---------scatter plot-------
+x = np.random.normal(5.0, 1.0, 1000)
+y = np.random.normal(10.0, 2.0, 1000)
+plt.scatter(x, y)
+plt.show()
+
+# --------linear regression-------
+from scipy import stats
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+print(stats.linregress(x,y))
+def myfunc(x):
+    return slope * x+ intercept
+myModel = list(map(myfunc,x))
+plt.scatter(x,y)
+plt.plot(x,myModel)
+plt.show()
